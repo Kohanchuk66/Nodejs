acdbb7057b555b7236006b0ed99e757e224eb40e
Illya <drozd.vol@gmail.com>
2023-06-26 23:28:12 +0300

    Added user validation middleware

--- middlewares/user.validation.middleware.js
+++ middlewares/user.validation.middleware.js
@@ -1,12 +1,87 @@
 import { USER } from "../models/user.js";
+import { userService } from "../services/userService.js";
 
 const createUserValid = (req, res, next) => {
   // TODO: Implement validatior for USER entity during creation
+  const {firstName, lastName, email, phoneNumber, password} = req?.body;
+
+  if(
+      !firstName || 
+      !lastName || 
+      !email || 
+      !phoneNumber || 
+      !password
+    ) {
+    res.status(400).send({
+      error: true,
+      message: 'User entity to create isn’t valid'
+  });
+  }
+
+  if(
+      !userService.checkEmail(email) || 
+      !userService.checkPassword(password) || 
+      !userService.checkPhoneNumber(phoneNumber)
+    ) {
+      res.status(400).send({
+        error: true,
+        message: 'User data isn’t valid'
+    });
+  }
+
+  if( userService.search({email: email}) ) {
+    res.status(400).send({
+      error: true,
+      message: 'This email is already associated with an account.'
+    });
+  }
+
+  if( userService.search({phoneNumber: phoneNumber}) ) {
+    res.status(400).send({
+      error: true,
+      message: 'This phone number is already associated with an account.'
+    });
+  }
+
   next();
 };
 
 const updateUserValid = (req, res, next) => {
   // TODO: Implement validatior for user entity during update
+  const {firstName, lastName, email, phoneNumber, password} = req?.body;
+
+  if(!firstName && !lastName && !email && !phoneNumber && !password){
+    res.status(400).send({
+      error: true,
+      message: 'User entity to create isn’t valid'
+  });
+  }
+
+  if(
+      email && !userService.checkEmail(email) || 
+      password && !userService.checkPassword(password) || 
+      phoneNumber && !userService.checkPhoneNumber(phoneNumber)
+    ) {
+      res.status(400).send({
+        error: true,
+        message: 'User data isn’t valid'
+    });
+  }
+
+  if( email && userService.search({email: email}) ) {
+    res.status(400).send({
+      error: true,
+      message: 'This email is already associated with an account.'
+    });
+  }
+
+  if( phoneNumber && userService.search({phoneNumber: phoneNumber}) ) {
+    res.status(400).send({
+      error: true,
+      message: 'This phone number is already associated with an account.'
+    });
+  }
+
   next();
 };
 
--- services/userService.js
+++ services/userService.js
@@ -2,6 +2,8 @@ import { userRepository } from "../repositories/userRepository.js";
 
 class UserService {
   // TODO: Implement methods to work with user
+  phonePattern = "^+380\d{3}\d{2}\d{2}\d{2}$";
+  emailPattern = "[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@gmail.com";
 
   search(search) {
     const item = userRepository.getOne(search);
@@ -9,6 +11,18 @@ class UserService {
       return null;
     }
     return item;
+  }
+
+  checkPhoneNumber(phoneNumber) {
+    return phonePattern.test(phoneNumber);
+  }
+
+  checkEmail(email) {
+    return emailPattern.test(email);
+  }
+
+  checkPassword(password) {
+    return !!(typeof password === 'string' && password.length >= 3);
   }
 }
 
