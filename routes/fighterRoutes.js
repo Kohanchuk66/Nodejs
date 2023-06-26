c8a7095360f0e49cb86ecfb6b45ecf50584e8284
Illya <drozd.vol@gmail.com>
2023-06-27 00:33:50 +0300

    Added fighter routes

--- routes/fighterRoutes.js
+++ routes/fighterRoutes.js
@@ -9,5 +9,52 @@ import {
 const router = Router();
 
 // TODO: Implement route controllers for fighter
+router.get("/", function(req, res, next) {
+    const fighters = fighterService.getFighters();
+
+    if(!fighters){
+      res.status(404).send('Fighters not found');
+    }
+
+    if(fighters?.length === 0){
+      res.status(204).send('No data');
+    }
+
+    res.send(`Fighters: ${users}`);
+  }
+);
+
+router.get("/:id", function(req, res, next) {
+      const fighter = fighterService.search(req.params.id);
+
+      res.send(`Fighter: ${fighter}`);
+    }
+);
+
+router.post("/",
+              createFighterValid,
+              function(req, res, next) {
+                fighterService.createFighter(req.body);
+
+              res.send(`Fighter created successfully`);
+              }
+            );
+
+router.put("/:id",
+            updateFighterValid,
+            function(req, res, next) {
+              fighterService.updateFighter(req.params.id, req.body);
+
+            res.send(`Fighter updated successfully`);
+            }
+          );
+
+router.delete("/:id",
+                function(req, res, next) {
+                  fighterService.deleteFighter(req.params.id);
+
+                  res.send(`Fighter deleted successfully`);
+                }
+              );
 
 export { router };
